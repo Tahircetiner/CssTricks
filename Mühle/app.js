c4fd12ce2b717ef3602ca4d0e29b3712452edf7f
Tahircetiner <35774187+Tahircetiner@users.noreply.github.com>
2020-03-12 16:59:39 +0100

    Add files via upload

--- Mühle/app.js
+++ Mühle/app.js
@@ -1,25 +1,42 @@
-var ziehen = document.getElementById("drag");
+var firstStones = document.getElementsByClassName("firstStone");
+var areaOne = document.getElementsByClassName("area");
+var itemIndex = 0;
 
-var pixel = 20;
-var img = document.getElementById("img");
-var canvas = document.createElement("canvas");
-canvas.width = img.width;
-canvas.height = img.height;
-canvas.getContext("2d").drawImage(img,0,0,img.width,img.height);
-canvas.getContext("2d").drawImage(ziehen,30,30,ziehen.width,ziehen.height);
-canvas.setAttribute("id","dropzone")
-document.body.appendChild(canvas);
+window.onclick = function (clickEvent) {
+    console.log("Geklicktes Element auf der x-Achse an der Position: " + clickEvent.pageX);
+    console.log("Geklicktes Element auf der y-Achse an der Position: " + clickEvent.pageY);
 
-var dropzone = document.getElementById("dropzone");
-ziehen.ondragstart = function (ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+    console.log(checkValidSetting(clickEvent.pageX,clickEvent.pageY, areaOne.item(i).getAttribute("coords").substring(0,2), areaOne.item(i).getAttribute("coords").substring(3,5)));
+
+    console.log("### " + areaOne.item(0).getAttribute("coords").substring(0,3));
+    console.log("### " + areaOne.item(0).getAttribute("coords").substring(4,7));
+
+
+        for(var i = 0; i < areaOne.length; i++){
+
+        if(checkValidSetting(clickEvent.pageX,clickEvent.pageY, areaOne.item(i).getAttribute("coords").substring(0,3), areaOne.item(i).getAttribute("coords").substring(4,7))){
+            setStone(firstStones.item(itemIndex),areaOne.item(i));
+            itemIndex++;
+        }
+    }
+
+    //setStone(firstStones.item(itemIndex),areaOne.item(itemIndex));
+    console.log("Wird hochgezaehlt?" + itemIndex);
+
 };
 
-dropzone.ondragover = function (ev) {
-   ev.preventDefault()
-};
+function setStone(stone,area) {
 
-dropzone.ondrop = function (ev) {
-    ev.preventDefault();
-    dropzone.appendChild(ziehen);
-};
+    stone.style.position = "absolute";
+    stone.style.left = area.getAttribute("coords").substring(0,3) + "px";
+    stone.style.top = area.getAttribute("coords").substring(4,7) + "px";
+}
+
+function checkValidSetting(pixelOfClickedElementX,pixelOfClickedElementY,pixelOfFieldX,pixelOfFieldY) {
+    var isValid = false;
+    if(pixelOfClickedElementX <= pixelOfFieldX && pixelOfClickedElementY <= pixelOfFieldY){
+        return true
+    }
+
+    return isValid;
+}
